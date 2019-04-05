aafcfa797c35e5db54ecbd6693702a7532ba786b
Patrick <icodeforyou@yahoo.com>
2019-04-04 22:02:56 -0500

    jpa finished

--- src/main/java/com/drcode/companydemo/entity/Employee.java
+++ src/main/java/com/drcode/companydemo/entity/Employee.java
@@ -1,14 +1,14 @@
 package com.drcode.companydemo.entity;
 
 import java.util.Date;
-import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -17,18 +17,20 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 @Entity
-@Table(name="department")
-public class Department {
-	
+@Table(name="employee")
+public class Employee {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
 	
 	private String name;
-	private String description;
+	private String title;
+	private Integer age;
+	private Date birthDate;
 	
-	@OneToMany(mappedBy="department")
-	private Set<Employee> employees;
+	@ManyToOne
+	@JoinColumn(name="department_id")
+	private Department department;
 	
 	@CreationTimestamp
 	@Temporal(TemporalType.TIMESTAMP)
@@ -39,24 +41,53 @@ public class Department {
 	@Temporal(TemporalType.TIMESTAMP)
 	@Column(name = "updateTs")
 	private Date modifyDate;
-	
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	public String getDescription() {
-		return description;
-	}
-	public void setDescription(String description) {
-		this.description = description;
+
+	public String getTitle() {
+		return title;
 	}
 
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public Date getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+
+	public Date getModifyDate() {
+		return modifyDate;
+	}
+
+	public void setModifyDate(Date modifyDate) {
+		this.modifyDate = modifyDate;
+	}
+	
 }
